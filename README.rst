d7aae90cf23649c335860ba647b596c9c8732359
weijie <wejie00@foxmail.com>
2019-07-16 17:27:09 +0800

    Add k8s volume json patch

---
+++ README.rst
@@ -0,0 +1,7 @@
+Aliyun plugin for `Tutor <https://docs.tutor.overhang.io>`_
+============================================================
+
+check kustomization.yml file
+```
+kubectl kustomize "$(tutor config printroot)/env"
+```
---
+++ setup.py
@@ -0,0 +1,40 @@
+import io
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, "README.rst"), "rt", encoding="utf8") as f:
+    readme = f.read()
+
+
+setup(
+    name="tutor-aliyun",
+    version="0.0.1",
+    url="",
+    project_urls={
+        "Documentation": "",
+        "Code": "https://github.com/WeJie/tutor-aliyun",
+        "Issue tracker": "https://github.com/WeJie/tutor-aliyun/issues",
+        "Community": "",
+    },
+    license="AGPLv3",
+    author="",
+    author_email="",
+    description="",
+    long_description=readme,
+    packages=["tutoraliyun"],
+    include_package_data=True,
+    python_requires=">=3.5",
+    entry_points={"tutor.plugin.v0": ["aliyun = tutoraliyun.plugin"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
